f6f516c955787b23bb0f443223c7f3d533825004
JNun0 <joao.nuno669@outlook.pt>
2022-03-17 14:32:41 +0000

    Finished

---
+++ Game1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameLoop
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        Color fundo = Color.Black;
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            if (state.IsKeyDown(Keys.S))
+            {
+                fundo=Color.White;
+            }
+            else if (state.IsKeyDown(Keys.D))
+            {
+                fundo = Color.Green;
+            }
+            else if (state.IsKeyDown(Keys.A))
+            {
+                fundo = Color.Red;
+            }
+            else if (state.IsKeyDown(Keys.W))
+            {
+                fundo = Color.Blue;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(fundo);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
